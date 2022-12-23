b8111b03c59d2354c81ea2e4d1c3e0591852dbbc
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-12-23 16:48:59 +0200

    edit button

--- src/components/phonebook/Phonebook.styled.jsx
+++ src/components/phonebook/Phonebook.styled.jsx
@@ -39,10 +39,11 @@ export const PhonebookInput = styled.input`
 export const PhonebookButton = styled.button`
     width: 100px;
     padding: 5px;
-    border: 1px solid #000;
-    border-radius: 5px;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+    height: 30px;
     background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+    border: none;
     cursor: pointer;
     transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
     &:hover,
@@ -50,7 +51,6 @@ export const PhonebookButton = styled.button`
         background-color: #f5f5f5;
         transform: scale(1.03);
         outline: none;
-    }
 `;
 
 export const PhonebookTitle = styled.h2`
